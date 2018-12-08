6b85363a7917b0a6038033e721ba9eb09152225c
Qendolin <32160662+Qendolin@users.noreply.github.com>
2018-12-08 13:31:51 +0100

    Deleted old Project and created a new one.

--- LEGO/src/bots/mark_1/Controller.java
+++ LEGO/src/bots/mark_1/Controller.java
@@ -2,27 +2,63 @@ package bots.mark_1;
 
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTRegulatedMotor;
 import lejos.robotics.navigation.DifferentialPilot;
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
 
 public class Controller {
-	
+
 	private DifferentialPilot pilot;
-	
+
 	public Controller() {
-		pilot = new DifferentialPilot(4, 9, Motor.B, Motor.C);
+		pilot = new DifferentialPilot(4, 14.5, Motor.B, Motor.C);
+	}
+
+	public void run() {
+		/*
+		 * pilot.travel(20, true); while (pilot.isMoving()) {
+		 * 
+		 * } System.out.println(" "+pilot.getMovement().getDistanceTraveled());
+		 * Button.waitForAnyPress();
+		 */
+		/*
+		 * for(int i = 0; i<4 ; i++) { pilot.travel(20); pilot.rotate(90); }
+		 */
+		LightSensor right = new LightSensor(SensorPort.S1);
+		LightSensor left = new LightSensor(SensorPort.S2);
+		//UseClaw(MotorPort.A,false);
+		//UseClaw(MotorPort.A,true);
+		//System.exit(0);
+		pilot.setTravelSpeed(17);
+		rideOn();
+		while (true) {
+			LCD.drawString("left:" + left.getNormalizedLightValue(), 0, 0);
+			LCD.drawString("right:" + right.getNormalizedLightValue(), 0, 1);
+			while (right.getNormalizedLightValue() < 400) {
+				pilot.rotate(-1, true);}
+			rideOn();
+			while (left.getNormalizedLightValue() < 400) {
+				pilot.rotate(1, true);}
+			rideOn();
+			if (Button.readButtons() != 0) {
+				System.exit(0);
+			}
+		}
+	}
+	public void rideOn() {
+		
+		pilot.travel(10000,true);
 	}
 	
-	public void run() {
-		/*pilot.travel(20, true);
-	    while (pilot.isMoving()) {
-
-	    }
-	    System.out.println(" "+pilot.getMovement().getDistanceTraveled());
-	    Button.waitForAnyPress();*/
-		for(int i = 0; i<4 ; i++)
-        {
-            pilot.travel(20);
-            pilot.rotate(90);                 
-        }
+	public void UseClaw(MotorPort motor,boolean close) {
+		motor.controlMotor(10, 1);
+		NXTRegulatedMotor m =new NXTRegulatedMotor(motor);
+		m.setSpeed(60);
+		if(close)m.rotate(-90);
+		else m.rotate(90);
+		m.waitComplete();
 	}
 }
---
+++ LEGO/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+import bots.mark_1.Controller;
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+public class Main {
+
+	public static void main(String[] args) {
+		Controller c = new Controller();
+		c.run();
+	}
+
+}
